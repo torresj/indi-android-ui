a65e2db6c1784a6a9e0a0da43fcd1ef498d6fb3d
Jaime Torres <jtbenavente@gmail.com>
2015-01-24 16:44:37 +0100

    gradle and android studio proyect version changed. APP test upadated

--- Android Test APP/Indi test/INDIserverTest/app/src/main/java/com/example/jaime/indiservertest/IndiClient.java
+++ Android Test APP/Indi test/INDIserverTest/app/src/main/java/com/example/jaime/indiservertest/IndiClient.java
@@ -1,5 +1,6 @@
 package com.example.jaime.indiservertest;
 
+
 import java.io.IOException;
 import java.util.Date;
 
@@ -9,63 +10,18 @@ import laazotea.indi.client.INDIProperty;
 import laazotea.indi.client.INDIPropertyListener;
 import laazotea.indi.client.INDIServerConnection;
 import laazotea.indi.client.INDIServerConnectionListener;
+import laazotea.indi.Constants;
 
 /**
  * Created by jaime on 20/01/2015.
  */
-public class IndiClient implements INDIServerConnectionListener, INDIDeviceListener, INDIPropertyListener, Runnable {
+public class IndiClient implements INDIServerConnectionListener, INDIDeviceListener, INDIPropertyListener{
 
     private INDIServerConnection connection;
-    private String host;
-    private int port;
+    private String log;
 
     public IndiClient(String host, int port){
-        this.host=host;
-        this.port=port;
-    }
-
-    @Override
-    public void newProperty(INDIDevice device, INDIProperty property) {
-
-    }
-
-    @Override
-    public void removeProperty(INDIDevice device, INDIProperty property) {
-
-    }
-
-    @Override
-    public void messageChanged(INDIDevice device) {
-
-    }
-
-    @Override
-    public void propertyChanged(INDIProperty property) {
-
-    }
-
-    @Override
-    public void newDevice(INDIServerConnection connection, INDIDevice device) {
-
-    }
-
-    @Override
-    public void removeDevice(INDIServerConnection connection, INDIDevice device) {
-
-    }
-
-    @Override
-    public void connectionLost(INDIServerConnection connection) {
-
-    }
-
-    @Override
-    public void newMessage(INDIServerConnection connection, Date timestamp, String message) {
-
-    }
-
-    @Override
-    public void run() {
+        log="";
         connection = new INDIServerConnection(host, port);
 
         connection.addINDIServerConnectionListener(this);  // We listen to all server events
@@ -74,8 +30,65 @@ public class IndiClient implements INDIServerConnectionListener, INDIDeviceListe
             connection.connect();
             connection.askForDevices();  // Ask for all the devices.
         } catch (IOException e) {
-            System.out.println("Problem with the connection: " + host + ":" + port);
+            log+="Problem with the connection: " + host + ":" + port+"\n";
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void newDevice(INDIServerConnection connection, INDIDevice device) {
+        // We just simply listen to this Device
+        log+="New device: " + device.getName()+"\n";
+        try {
+            device.BLOBsEnable(Constants.BLOBEnables.ALSO); // Enable receiving BLOBs from this Device
+        } catch (IOException e) {
+        }
+        device.addINDIDeviceListener(this);
+    }
+
+    @Override
+    public void removeDevice(INDIServerConnection connection, INDIDevice device) {
+        // We just remove ourselves as a listener of the removed device
+        log+="Device Removed: " + device.getName()+"\n";
+        device.removeINDIDeviceListener(this);
+    }
+
+    @Override
+    public void connectionLost(INDIServerConnection connection) {
+        log+="Connection lost. Bye"+"\n";
+
+    }
+
+    @Override
+    public void newMessage(INDIServerConnection connection, Date timestamp, String message) {
+        log+="New Server Message: " + timestamp + " - " + message+"\n";
+    }
+
+    @Override
+    public void newProperty(INDIDevice device, INDIProperty property) {
+        // We just simply listen to this Property
+        log+="New Property (" + property.getName() + ") added to device " + device.getName()+"\n";
+        property.addINDIPropertyListener(this);
+    }
+
+    @Override
+    public void removeProperty(INDIDevice device, INDIProperty property) {
+        // We just remove ourselves as a listener of the removed property
+        log+="Property (" + property.getName() + ") removed from device " + device.getName()+"\n";
+        property.removeINDIPropertyListener(this);
+    }
+
+    @Override
+    public void messageChanged(INDIDevice device) {
+        log+="New Device Message: " + device.getName() + " - " + device.getTimestamp() + " - " + device.getLastMessage()+"\n";
+    }
+
+    @Override
+    public void propertyChanged(INDIProperty property) {
+        log+="Property Changed: " + property.getNameStateAndValuesAsString()+"\n";
+    }
+
+    public String getLog(){
+        return log;
     }
 }
--- Android Test APP/Indi test/INDIserverTest/app/src/main/java/com/example/jaime/indiservertest/MyActivity.java
+++ Android Test APP/Indi test/INDIserverTest/app/src/main/java/com/example/jaime/indiservertest/MyActivity.java
@@ -1,30 +1,34 @@
 package com.example.jaime.indiservertest;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends ActionBarActivity {
 
     private IndiClient client;
-    private Thread t;
+    private TextView logView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        logView=(TextView)findViewById(R.id.log);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.menu_my, menu);
         return true;
     }
 
@@ -34,31 +38,60 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
     public void clickConn(View v) {
         if(((Button)v).getText().toString().equals("Connect")) {
-            EditText SERVER_IP   = (EditText)findViewById(R.id.server);
-            EditText SERVER_PORT   = (EditText)findViewById(R.id.port);
-            String server=SERVER_IP.getText().toString();
-            int port=0;
-            try {
-                port = Integer.parseInt(SERVER_PORT.getText().toString());
-            } catch (NumberFormatException e) {
-
-            }
-            client=new IndiClient(server,port);
-            t=new Thread(client);
-            t.start();
-
+            new UpdateLog().execute();
             ((Button) v).setText("Disconnect");
         }else{
             ((Button) v).setText("Connect");
 
         }
+    }
+
+    class UpdateLog extends AsyncTask<Void, String, Void> {
+
+        private int port;
+        private String host;
+
+
+        @Override protected void onPreExecute() {
+            EditText SERVER_IP   = (EditText)findViewById(R.id.server);
+            EditText SERVER_PORT   = (EditText)findViewById(R.id.port);
+            host=SERVER_IP.getText().toString();
+            port=0;
+            try {
+                port = Integer.parseInt(SERVER_PORT.getText().toString());
+            } catch (NumberFormatException e) {
+
+            }
+            logView.setText("Connecting...");
+        }
+
+        @Override protected Void doInBackground(Void... par) {
+            client=new IndiClient(host,port);
+            while(true){
+                publishProgress(client.getLog());
+                SystemClock.sleep(1000);
+            }
+        }
+
+        @Override protected void onProgressUpdate(String... prog) {
+            logView.setText(prog[0]);
+        }
+
+        @Override protected void onPostExecute(Void result) {
+
+
+        }
+
     }
 }
